4d3fa91871d402a1971cb7ce4870bd634f32c030
Sathvik-Malgikar <Sathvik.malgikar@gmail.com>
2023-04-07 03:09:48 +0530

    jackie

--- standalones/timeline.py
+++ standalones/timeline.py
@@ -1,15 +1,16 @@
 from time import sleep
-print("enter space/folder name")
+# print("enter space/folder name")
 # folder_name=input()
-folder_name = "BlenderWorkspace"
 import os,requests
 
 
 while True:
     sleep(3)
 
-    print ( "reading files in ", folder_name)
-    # os yet to do
-    resp = requests.post("http://172.16.128.189:3000/sync", data={"spacename" : folder_name}, files={"data":open('cuterobot.jpg','rb')}  )
-    print(resp.text)
+    print ( "reading files in current directory",os.pardir)
+    all  = os.listdir()
+    for file in all:
+        print(file.title())
+        resp = requests.post("http://172.16.128.95:3001/sync", data={"spacename" : os.pardir}, files={"data":open(file.title(),'rb')}  )
+        print(resp.text)
     
