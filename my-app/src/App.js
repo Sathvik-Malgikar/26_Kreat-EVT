c8e3dffcae4f64b0c65f8699c6c844794fda12fc
Sathvik-Malgikar <Sathvik.malgikar@gmail.com>
2023-04-06 19:33:11 +0530

    routing

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,14 @@
 
+import { Link } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Link to={"standalone"} >
+          <p>Get standalone software</p>
+        </Link>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ my-app/src/GetStandalone.js
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+import "./stndstyles.css"
+
+export default function GetStandalone(){
+    return<div className="Flex">
+      <Link to={"/"} >
+          <p>Go back to home page</p>
+        </Link>
+        <p className="Msg" >Get the standalone exe run it from your space to start building the Timeline</p>
+        <br></br>
+        <div className="Button" >Download for windows 64-bit</div>
+    </div>
+}
