6e5142c47b37d3b7d8872a7517c96e8d15e3472f
XeroMorphinG <97865060+XeroMorphinG@users.noreply.github.com>
2023-04-06 23:58:41 +0530

    asflasf

--- my-app/src/Register.jsx
+++ my-app/src/Register.jsx
@@ -1,7 +1,8 @@
-import './output.css';
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { useState } from "react"
+import './input.css'
+
 
 export default function Register(){
 const [profname, setprofname] = useState("")
@@ -16,16 +17,22 @@ const [file, setFile] = useState("")
     // function handleChange(event) {
     //     setFile(event.target.files[0])
     //   }
-return (<div className="w-screen h-screen BODY" >
+    return (
+    <div className="h-screen dark:bg-gray-900 text-center" id='register' >
+    
+        <form action="http://172.16.128.189:3000/profilePage" className='max-w-sm rounded overflow-hidden shadow-lg'>
+        <h1 className='text-white text-5xl' id = "registerHead">Register</h1>
+        <label className='text-white'>Username : </label><input name="profilename"  onChange={setprofname} className='py-20'></input><br></br><br></br>
+        <label className='text-white'>Password : </label><input name="password"  type="password" onChange={setpwd}className='py-20' ></input><br></br><br></br>
+        <label className='text-white'>Email ID : </label><input  name="email" type="email" onChange={setemail} className='py-20'></input><br></br><br></br>
+        <label className='text-white'>Profile Picture : </label><input name="photo"  type="file" ></input><br></br><br></br>
+        <button type="submit" className='blue-700 text-white font-bg-white hover:bg-gray-100 text-gray-900 font-semibold py-2 px-4 border border-gray-400 rounded shadowbold py-2 px-4 rounded-full'>Submit</button>
+    </form>
 
-<form action="http://172.16.128.189:3000/profilePage" >
-<input name="profilename"  onChange={setprofname} ></input>
-<input name="password"  type="password" onChange={setpwd} ></input>
-<input  name="email" type="email" onChange={setemail} ></input>
-<input name="photo"  type="file" ></input>
-<button type="submit" >submit</button>
-</form>
-</div>)
+
+
+
+    </div>)
 
 
 }
